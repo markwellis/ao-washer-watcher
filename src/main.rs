7d39be1476a6ce17cce112add72fed4b2c5ad8e5
Mark Ellis <m@rkellis.com>
2021-01-15 21:13:12 +0000

    works

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,29 @@ extern crate select;
 
 use select::document::Document;
 use select::predicate::Class;
-use anyhow::{Result, anyhow};
 
 const L8_URL: &str = "https://ao.com/product/l8wec166r-aeg-okomix-technology-washer-dryer-white-45659-2.aspx";
 const L9_URL: &str = "https://ao.com/product/l9wec169r-aeg-sensidry-technology-washer-dryer-white-45660-2.aspx";
 
 #[tokio::main]
 async fn main() {
-    //loop 
-        match find_add_to_basket(L8_URL).await {
-            Err(e) => eprintln!("{}", e),
-            Ok(v) => if v {println!("IN STOCK {}", L8_URL)}
-        }
-
-        match find_add_to_basket(L9_URL).await {
-            Err(e) => eprintln!("{}", e),
-            Ok(v) => if v {println!("IN STOCK {}", L9_URL)}
-        }
-    //sleep
+    find_add_to_basket(L8_URL).await;
+    find_add_to_basket(L9_URL).await;
 }
 
-async fn find_add_to_basket(url: &str) -> Result<bool> {
-    let resp = reqwest::get(L8_URL).await?;
+async fn find_add_to_basket(url: &str) {
+    let res = reqwest::get(url).await.unwrap();
 
-    if resp.status().is_success() {
-        return Err(anyhow!("request failed for {}", url));
+    if !res.status().is_success() {
+        panic!("request failed for {}", url);
     }
+    let path = res.url().path().to_string();
 
-    let document = Document::from(resp.text().await?.as_str());
+    let document = Document::from(res.text().await.unwrap().as_str());
 
     let button = document.find(Class("addToBasket"));
 
-    println!("c {}", button.count());
-
-    // if !button.is_empty() {
-    //     return Ok(true);
-    // }
-
-    return Ok(false)
+    if button.count() > 0 {
+        println!("IN STOCK {}", path);
+    }
 }
